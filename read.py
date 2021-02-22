64aea72fa9779e5796f4b1d8ef0dd1659b7f4ce7
Jocelyn Liu <bettyliu840727@gmail.com>
2021-02-22 13:37:48 +0800

    add word count

--- read.py
+++ read.py
@@ -21,3 +21,38 @@ for d in data:
 print('一共有', len(new), '留言長度小於100')
 print(new[0])
 print(new[1])
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有', len(good), '筆留言提到good')
+
+good = [d for d in data if 'good' in d]
+
+#文字計數
+wc = {} #word_count
+for d in data:
+	words = d.split()
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1 #新增key進字典
+
+for word in wc:
+	if wc[word] > 1000000:
+		print(word, wc[word])
+
+print(len(wc))
+print(wc['Jocelyn'])
+
+while True:
+	word = input('請問您要查甚麼字: ')
+	if word == 'q':
+		break
+	if word in wc:
+		print(word, '出現的次數為: ', wc[word])
+	else:
+		print('這個字沒有出現過喔')
+print('感謝使用此留言查詢功能')
